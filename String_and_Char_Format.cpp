a8f0866446341371bf11917e3e425361ad7dd244
YOUSSEF-code <136598297+YOSSEF-cod@users.noreply.github.com>
2025-03-31 18:16:55 +0000

    updar

---
+++ String_and_Char_Format.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    char Name[] = "youssef ";
+    // print string and String 
+    printf("Dear %s, How are you ?", Name);
+
+    char c = 'S';
+    printf("Setting the width of c :%*c\n", 1, c);
+    printf("Setting the width of c :%*c\n", 2, c);
+    printf("Setting the width of c :%*c\n", 3, c);
+    printf("Setting the width of c :%*c\n", 5, c);
+
+}
