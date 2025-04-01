d1c20b389dbc0af85dc243d4a1ef677b04eb5c5c
YOUSSEF-code <136598297+YOSSEF-cod@users.noreply.github.com>
2025-04-01 20:18:22 +0000

    updat

---
+++ Setw_Manipulator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+
+    cout << "---------|----------------------------------|---------|" << endl;
+    cout << "   Code  |                Name              |  Mark   |"<< endl;
+    cout << "---------|----------------------------------|---------|" << endl;
+    
+    cout << setw(9) << "C101" << "|" << setw(34) << "introduction to Programming 1 " <<"|" << setw(9) << "95" << "|" << endl;
+    cout << setw(9) << "C102" << "|" << setw(34) << "Computer Hardware" << "|" << setw(9) << "88" << "|" << endl;
+    cout << setw(9) << "C1035244" << "|" << setw(34) << "Netwerk" << "|" << setw(9) << "75" << "|" << endl;
+
+    cout << "---------|----------------------------------|---------|" << endl;
+}
