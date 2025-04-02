f2f176b986df0b68f4e24acc2082c3bf27c0c8ef
YOUSSEF-code <136598297+YOSSEF-cod@users.noreply.github.com>
2025-04-02 22:53:37 +0000

    updat

---
+++ Vectors_Part_I/AddElementVector.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <int> vNumber;
+
+    vNumber.push_back(10);
+    vNumber.push_back(20);
+    vNumber.push_back(30);
+    vNumber.push_back(40);
+    vNumber.push_back(50);
+
+    cout << "Number Vector : \n";
+    for (int &Number : vNumber)
+    {
+        cout << Number << " ";
+    }
+}
---
+++ Vectors_Part_I/Vectors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // vector is a dynamic
+    // std::vector <Type> vector_Name
+    vector <int> vNumber = { 3, 5, 9, 10, 18};
+
+    cout << "Number Vector : ";
+    // ranged loop
+    for (int &Number : vNumber)
+    {
+        cout << Number << " ";
+    }
+    cout << endl;
+}
