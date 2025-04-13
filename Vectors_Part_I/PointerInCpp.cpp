c2f99b0ea424cab21d376766c205663c3d7586dc
youssef bounite <136598297+ybounite@users.noreply.github.com>
2025-04-13 17:51:50 +0000

    exerces of pointer

---
+++ Vectors_Part_I/PointerInCpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int a = 10;
+
+
+    cout << "a value  : " << a << endl;
+    cout << "a address: " << &a << endl;
+
+    int *p = &a;
+
+    cout << "Pointer Value : " << p << endl;
+    cout << "print Value of Pointer : " << *p;
+    cout << endl;
+}
