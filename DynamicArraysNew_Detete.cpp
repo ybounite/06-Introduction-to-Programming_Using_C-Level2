24b751d32e329134d3b813edffd9e1701fe50e43
youssef bounite <136598297+ybounite@users.noreply.github.com>
2025-04-20 08:42:06 +0000

    memory allocation

--- DynamicArraysNew_Detete.cpp
+++ DynamicArraysNew_Detete.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-    
+    float *ptr;
+    int length;
+
+    cout << "Enter total number of students.\n";
+    cin >> length;
+
+    // memeory allocation of length of floats
+    ptr = new float[length];
+
+    cout << "Enter grades of students." << endl;
+    for (int i = 0; i < length; i++)
+    {
+        cout << "Student [ " << i + 1 << " ] : " << endl;
+        cin >> ptr[i];
+    }
+    cout << "\n Displaying grades of students." << endl;
+    for (int i = 0; i < length; i++){
+        cout << "Student [ " << i + 1 << " ] : " << ptr[i] << endl;
+    }
+    // ptr memory is released 
+    delete[] ptr;
 }
