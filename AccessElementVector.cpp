d4644d33ea3c0a5e7ff4e226f7930d66e4c4fb56
youssef bounite <136598297+ybounite@users.noreply.github.com>
2025-04-20 18:21:48 +0000

    Accesse in Element Vector

---
+++ AccessElementVector.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <string> cars = {"Volvo", "BMw", "Ford", "Mazda"};
+
+    // Get the secode element 
+    cout << cars.at(1) << endl;
+}
