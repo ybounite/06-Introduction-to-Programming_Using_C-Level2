c0cdc909defc5f3494fea44dc3342cfc137dc607
youssef bounite <136598297+ybounite@users.noreply.github.com>
2025-05-04 05:58:12 +0000

    example for exception handling

---
+++ ExceptionHandling/ExceptionHandling.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/*Exceptions provide a way to react to exceptional circumstances (like runtime errors)
+ in programs by transferring control to special functions called handlers.*/'
+
+int main()
+{
+    vector <int> num{1, 2, 3, 4, 5};
+
+    try
+    {
+        cout << num.at(5);
+    }
+    catch (...)
+    {
+        cout << "out of bound\n" << endl;
+    }
+}
